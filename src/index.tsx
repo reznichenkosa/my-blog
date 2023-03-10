17e15ab4bde3fe63f92b0175b78ddd06c1c05318
Reznichenko Sergey <sergiliocr@gmail.com>
2023-03-10 20:05:00 +0300

    feat: add react lib

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from "react-dom";
+import { Counter } from "./components/Counter";
+
+render(<div>test</div>, document.getElementById("root"));
--- webpack.config.ts
+++ webpack.config.ts
@@ -10,7 +10,7 @@ export default (env: BuildEnv) => {
   const PORT = env.port || 3000;
 
   const paths: BuildPaths = {
-    entry: path.resolve(__dirname, "src", "index.ts"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "build"),
     html: path.resolve(__dirname, "public", "index.html"),
   };
